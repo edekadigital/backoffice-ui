1b0ac7e9debd2bc0e4e385bf28373bb862339c1f
René Schapka <rene.schapka@edeka.de>
2019-04-18 12:21:47 +0200

    Fix property name type -> variant

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -6,34 +6,34 @@ import { Button, Delete, ArrowForward } from '..';
 storiesOf('Components|Button', module)
   .add('contained', () => <Button>Some Label</Button>)
   .add('contained with icon', () => (
-    <Button type="contained" icon={Delete}>
+    <Button variant="contained" icon={Delete}>
       Some Label
     </Button>
   ))
   .add('contained with icon (right)', () => (
-    <Button type="contained" icon={ArrowForward} iconPosition="right">
+    <Button variant="contained" icon={ArrowForward} iconPosition="right">
       Some Label
     </Button>
   ))
-  .add('text', () => <Button type="text">Some Label</Button>)
+  .add('text', () => <Button variant="text">Some Label</Button>)
   .add('text with icon', () => (
-    <Button type="text" icon={Delete}>
+    <Button variant="text" icon={Delete}>
       Some Label
     </Button>
   ))
   .add('text with icon (right)', () => (
-    <Button type="text" icon={ArrowForward} iconPosition="right">
+    <Button variant="text" icon={ArrowForward} iconPosition="right">
       Some Label
     </Button>
   ))
-  .add('outlined', () => <Button type="outlined">Some Label</Button>)
+  .add('outlined', () => <Button variant="outlined">Some Label</Button>)
   .add('outlined with icon', () => (
-    <Button type="outlined" icon={Delete}>
+    <Button variant="outlined" icon={Delete}>
       Some Label
     </Button>
   ))
   .add('outlined with icon (right)', () => (
-    <Button type="outlined" icon={ArrowForward} iconPosition="right">
+    <Button variant="outlined" icon={ArrowForward} iconPosition="right">
       Some Label
     </Button>
   ));
