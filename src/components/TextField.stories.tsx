1b01b117fd1b109a4d455b9c727fbc20bd6ddb94
René Schapka <rene.schapka@edeka.de>
2019-04-25 05:52:08 +0200

    Add TextField

---
+++ src/components/TextField.stories.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { TextField, Button } from '..'; // @edekadigital/backoffice-ui
+
+storiesOf('Components|TextField', module)
+  .add('default', () => <TextField label="Some label" />)
+  .add('required', () => <TextField label="Some label" required={true} />)
+  .add('with placeholder', () => (
+    <TextField label="Some label" placeholder="Placeholder" />
+  ))
+  .add('with value', () => <TextField label="Some label" value="Some value" />)
+  .add('password', () => (
+    <TextField label="Password" required={true} type="password" />
+  ));
---
+++ src/components/TextField.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { TextField as MuiTextField } from '@material-ui/core';
+
+export type TextFieldValue = string | number;
+
+export type TextFieldType =
+  | 'color'
+  | 'date'
+  | 'datetime-local'
+  | 'email'
+  | 'month'
+  | 'number'
+  | 'password'
+  | 'search'
+  | 'tel'
+  | 'text'
+  | 'time'
+  | 'url'
+  | 'week';
+
+export interface TextFieldProps {
+  label: string;
+  placeholder?: string;
+  type?: TextFieldType;
+  required?: boolean;
+  helperText?: string;
+  error?: boolean;
+  autoComplete?: string;
+  defaultValue?: TextFieldValue;
+  disabled?: boolean;
+  id?: string;
+  name?: string;
+  value?: TextFieldValue;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+export const TextField: React.FC<TextFieldProps> = props => {
+  const { label, type = 'text', required = false, ...additionalProps } = props;
+  const modifiedLabel = required ? label : `${label} (optional)`;
+  const InputLabelProps = {
+    shrink: true,
+    required: false,
+  };
+  return (
+    <MuiTextField
+      {...additionalProps}
+      label={modifiedLabel}
+      type={type}
+      required={required}
+      InputLabelProps={InputLabelProps}
+      fullWidth={true}
+    />
+  );
+};
