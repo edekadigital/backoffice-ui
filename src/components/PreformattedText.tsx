d4d6136a7d44947a16665b815922d7c2c1542144
René Schapka <rene.schapka@edeka.de>
2019-04-27 00:16:32 +0200

    Adjust font style

--- src/components/PreformattedText.tsx
+++ src/components/PreformattedText.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
+import { withStyles, createStyles, WithStyles, Theme } from '@material-ui/core';
 
 export interface PreformattedTextProps {
   children: React.ReactNode;
 }
 
-export const PreformattedText: React.FC<PreformattedTextProps> = ({
-  children,
-}) => {
-  return <pre>{children}</pre>;
+const preStyles = createStyles((theme: Theme) => ({
+  root: {
+    margin: 0,
+    fontFamily:
+      'Menlo, Consolas, "Liberation Mono", Monaco, "Lucida Console", monospace',
+    fontSize: '0.875rem',
+    lineHeight: 1.3,
+    color: theme.palette.text.primary,
+  },
+}));
+
+const PreformattedTextComponent: React.FC<
+  PreformattedTextProps & WithStyles
+> = ({ classes, children }) => {
+  return <pre className={classes.root}>{children}</pre>;
 };
+
+export const PreformattedText = withStyles(preStyles)(
+  PreformattedTextComponent
+);
