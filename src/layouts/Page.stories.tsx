984bcf0325fc5121170fcedf11e9d42c4889e8d0
René Schapka <rene.schapka@edeka.de>
2019-04-27 01:27:01 +0200

    Add top bar

--- src/layouts/Page.stories.tsx
+++ src/layouts/Page.stories.tsx
@@ -2,9 +2,10 @@ import * as React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { Page, Body } from '..'; // @edekadigital/backoffice-ui
+import { ReverseNavigation } from '../components/ReverseNavigation';
 
-const Content = () => (
-  <Body>
+const Paragraph1 = () => (
+  <p>
     Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>,
     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
     ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
@@ -12,6 +13,46 @@ const Content = () => (
     velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
     cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
     est laborum.
+  </p>
+);
+
+const Paragraph2 = () => (
+  <p>
+    Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus
+    accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus
+    et ultrices posuere cubilia Curae;
+    <strong>Sed aliquam, nisi quis porttitor</strong> congue, elit erat euismod
+    orci, ac placerat dolor lectus quis orci. Phasellus consectetuer vestibulum
+    elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.
+    Vestibulum fringilla pede sit amet augue. In turpis. Pellentesque posuere.
+    Praesent turpis. Aenean posuere, tortor sed cursus feugiat, nunc augue
+    blandit nunc, eu sollicitudin urna dolor sagittis lacus. Donec elit libero,
+    sodales nec, volutpat a, suscipit non, turpis. Nullam sagittis. Suspendisse
+    pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec
+    pellentesque velit pede quis nunc. Vestibulum ante ipsum primis in faucibus
+    orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Ut varius
+    tincidunt libero. Phasellus dolor. Maecenas vestibulum mollis.
+  </p>
+);
+
+const Content = () => (
+  <Body component="div">
+    <Paragraph1 />
+  </Body>
+);
+
+const ContentLong = () => (
+  <Body component="div">
+    <Paragraph1 />
+    <Paragraph2 />
+    <Paragraph1 />
+    <Paragraph2 />
+    <Paragraph1 />
+    <Paragraph2 />
+    <Paragraph1 />
+    <Paragraph2 />
+    <Paragraph1 />
+    <Paragraph2 />
   </Body>
 );
 
@@ -52,4 +93,38 @@ storiesOf('Layouts|Page', module)
       <Content />
       <Content />
     </Page>
-  ));
+  ))
+  .add('with ReverseNavigation', () => {
+    const handleBarBackClick = () => {
+      console.log('handleBarBackClick');
+    };
+
+    const bar = (
+      <ReverseNavigation onBackClick={handleBarBackClick}>
+        Lorem ipsum dolor <strong>sit amet</strong>
+      </ReverseNavigation>
+    );
+
+    return (
+      <Page bar={bar}>
+        <ContentLong />
+      </Page>
+    );
+  })
+  .add('with ReverseNavigation (sticky + narrow)', () => {
+    const handleBarBackClick = () => {
+      console.log('handleBarBackClick');
+    };
+
+    const bar = (
+      <ReverseNavigation variant="narrow" onBackClick={handleBarBackClick}>
+        Lorem ipsum dolor <strong>sit amet</strong>
+      </ReverseNavigation>
+    );
+
+    return (
+      <Page bar={bar} variant="narrow" stickyBar={true}>
+        <ContentLong />
+      </Page>
+    );
+  });
--- src/layouts/Page.tsx
+++ src/layouts/Page.tsx
@@ -12,6 +12,8 @@ export type PageVariant = 'default' | 'narrow';
 export type PageChildren = React.ReactNode | React.ReactNode[];
 
 export interface PageProps {
+  bar?: React.ReactNode;
+  stickyBar?: boolean;
   variant?: PageVariant;
   children: PageChildren;
 }
@@ -29,24 +31,39 @@ const pageStyles = (theme: Theme) =>
   createStyles({
     root: {
       boxSizing: 'border-box',
-      padding: spacing,
       minHeight: '100vh',
       background: theme.palette.grey[200],
     },
     outer: {
+      padding: spacing,
+    },
+    inner: {
       margin: '0 auto',
       width: '100%',
       maxWidth: 1400,
     },
-    outerNarrow: {
+    innerNarrow: {
       maxWidth: 800,
+    },
+    innerWithStickyBar: {
+      paddingTop: 70,
+    },
+    stickyBarWrapper: {
+      position: 'fixed',
+      width: '100vw',
     },
   });
 
 export const PageComponent: React.FC<PageProps & WithStyles> = props => {
-  const { variant = 'default', children, classes } = props;
+  const {
+    bar,
+    stickyBar = false,
+    variant = 'default',
+    children,
+    classes,
+  } = props;
+  const hasBar = !!bar;
   const items = Array.isArray(children) ? children : [children];
-
   const itemSizingProps: ItemSizingProps = {
     xs: false,
     md: true,
@@ -62,23 +79,32 @@ export const PageComponent: React.FC<PageProps & WithStyles> = props => {
     itemSizingProps.lg = true;
   }
 
-  let outerClassName = classes.outer;
-  if (variant === 'narrow') {
-    outerClassName = `${outerClassName} ${classes.outerNarrow}`;
-  }
-
   const gridContent = items.map((tempItem, index) => (
     <Grid item={true} key={`page-item-${index}`} {...itemSizingProps}>
       {tempItem}
     </Grid>
   ));
 
+  let innerClassName = classes.inner;
+  if (variant === 'narrow') {
+    innerClassName = `${innerClassName} ${classes.innerNarrow}`;
+  }
+  if (hasBar && stickyBar) {
+    innerClassName = `${innerClassName} ${classes.innerWithStickyBar}`;
+  }
+
+  const barWrapperClassName = stickyBar ? classes.stickyBarWrapper : undefined;
+  const top = hasBar ? <div className={barWrapperClassName}>{bar}</div> : null;
+
   return (
     <div className={classes.root}>
-      <div className={outerClassName}>
-        <Grid container={true} spacing={24} justify="center" component="main">
-          {gridContent}
-        </Grid>
+      {top}
+      <div className={classes.outer}>
+        <div className={innerClassName}>
+          <Grid container={true} spacing={24} justify="center" component="main">
+            {gridContent}
+          </Grid>
+        </div>
       </div>
     </div>
   );
