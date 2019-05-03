b634da963e6e635010a1335406cee790acdab67c
René Schapka <rene.schapka@edeka.de>
2019-05-03 11:53:28 +0200

    Extend story "Componets/Switch/controlled"

--- src/components/Switch.stories.tsx
+++ src/components/Switch.stories.tsx
@@ -5,9 +5,25 @@ import { Switch } from '..'; // @edekadigital/backoffice-ui
 
 storiesOf('Components|Switch', module)
   .add('default', () => <Switch label="Some label" value="someValue" />)
-  .add('controlled', () => (
-    <Switch label="Some label" value="someValue" checked={true} />
-  ))
   .add('disabled', () => (
     <Switch label="Some label" value="someValue" disabled={true} />
-  ));
+  ))
+  .add('controlled', () => {
+    const StoryComponent = () => {
+      const [checked, setChecked] = React.useState<boolean>(true);
+
+      const toggle = React.useCallback(() => {
+        setChecked(!checked);
+      }, [checked]);
+
+      return (
+        <Switch
+          label="Some label"
+          value="someValue"
+          checked={checked}
+          onChange={toggle}
+        />
+      );
+    };
+    return <StoryComponent />;
+  });
