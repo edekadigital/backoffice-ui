0c3c781d607a2c6a1a0cf7f05c2ae01791d46261
René Schapka <rene.schapka@edeka.de>
2020-01-31 13:56:43 +0100

    Change package structure

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,14 @@ export * from './components/AppBar';
 export * from './components/Button';
 export * from './components/ConfirmationDialog';
 export * from './components/ContentGroup';
-export * from './components/FormRow';
 export * from './components/IconButton';
 export * from './components/ReverseNavigation';
 export * from './components/SearchField';
 export * from './components/StatusChip';
 export * from './components/Switch';
 export * from './components/TextField';
+
+export * from './forms/FormRow';
 
 export * from './typography/Body';
 export * from './typography/Heading';
