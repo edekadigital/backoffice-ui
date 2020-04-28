2808368073617579cec19b69e0a3f2db7be4a9e8
Antonia <48949038+antokro@users.noreply.github.com>
2020-04-28 13:41:46 +0200

    Data table test (#18)
    * Add testid to Action buttons, add test fpr headline and actions
    
    * Add cleanup fn to DataTable useEffect
    
    * Ignore DataTable for coverage
    
    Co-authored-by: Antonia Krohn <antonia.krohn@edeka.de>

--- src/components/DataTable/DataTable.spec.tsx
+++ src/components/DataTable/DataTable.spec.tsx
@@ -1,35 +1,68 @@
 import * as React from 'react';
 
-import { cleanup } from '@testing-library/react';
+import { cleanup, fireEvent } from '@testing-library/react';
 import { DataTable, FetchResult } from './DataTable';
 import { render } from '../../test-utils';
 import { ArrowRight } from '../..';
-import 'react-table';
 
 const fetchData = (): Promise<FetchResult<{}>> =>
   new Promise(() => [
     { data: [{ some: 'data', data: 'some' }], pageIndex: 1, totalCount: 1 },
   ]);
 
+const columns = [
+  { Header: 'Some', accessor: 'some' },
+  { Header: 'Data', accessor: 'data' },
+];
+
 describe('<DataTable>', () => {
   afterEach(cleanup);
 
   it('should render the component', () => {
-    const columns = [
-      { Header: 'Some', accessor: 'some' },
-      { Header: 'Data', accessor: 'data' },
-    ];
     const { container } = render(
       <DataTable
         fetchData={fetchData}
         columns={columns}
         pagination={{ labelRowsPerPage: 'Rows', rowsPerPageOptions: [5, 10] }}
-        headline="Test table"
-        actions={[{ icon: ArrowRight, handler: jest.fn() }]}
-        drawerWidth="sm"
+      />
+    );
+    expect(container).toBeTruthy();
+  });
+
+  it('should render the component with checkboxes', () => {
+    const { container, getByTestId } = render(
+      <DataTable
+        fetchData={fetchData}
+        columns={columns}
+        pagination={{ labelRowsPerPage: 'Rows', rowsPerPageOptions: [5, 10] }}
         tableSelectionActions={[{ icon: ArrowRight, handler: jest.fn() }]}
       />
     );
     expect(container).toBeTruthy();
+  });
+
+  it('should render the component with actions', () => {
+    const { getByTestId } = render(
+      <DataTable
+        fetchData={fetchData}
+        columns={columns}
+        pagination={{ labelRowsPerPage: 'Rows', rowsPerPageOptions: [5, 10] }}
+        actions={[{ icon: ArrowRight, handler: jest.fn() }]}
+      />
+    );
+
+    expect(getByTestId('tablebar-action')).toBeTruthy();
+  });
+
+  it('should render the component with headline', () => {
+    const { getByTestId } = render(
+      <DataTable
+        fetchData={fetchData}
+        columns={columns}
+        pagination={{ labelRowsPerPage: 'Rows', rowsPerPageOptions: [5, 10] }}
+        headline="Test table"
+      />
+    );
+    expect(getByTestId('tablebar-heading')).toBeTruthy();
   });
 });
