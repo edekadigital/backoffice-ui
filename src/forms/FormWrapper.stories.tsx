783c2b00ab37c4a041f353690416c2c7766b16ac
Antonia Krohn <antonia.krohn@edeka.de>
2020-05-04 16:07:15 +0200

    Make Cancel button optional

--- src/forms/FormWrapper.stories.tsx
+++ src/forms/FormWrapper.stories.tsx
@@ -3,42 +3,58 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { FormWrapper, FormFieldSet, FormRow, TextField } from '..'; // @edekadigital/backoffice-ui
 
-storiesOf('Forms|FormWrapper', module).add('default', () => {
-  const onSubmit = () => {
-    console.log('onSubmit');
-  };
+const onSubmit = () => {
+  console.log('onSubmit');
+};
 
-  const onCancel = () => {
-    console.log('onCancel');
-  };
-
-  return (
-    <FormWrapper
-      submitLabel="Submit"
-      cancelLabel="Cancel"
-      onSubmit={onSubmit}
-      onCancel={onCancel}
-    >
-      <FormFieldSet title="Lorem ipsum">
-        <FormRow gutterBottom={true}>
-          <TextField label="Some Label" />
-          <TextField label="Some Label" />
-        </FormRow>
-        <FormRow>
-          <TextField label="Some Label" />
-          <TextField label="Some Label" />
-        </FormRow>
-      </FormFieldSet>
-      <FormFieldSet title="Lorem ipsum">
-        <FormRow gutterBottom={true}>
-          <TextField label="Some Label" />
-          <TextField label="Some Label" />
-        </FormRow>
-        <FormRow>
-          <TextField label="Some Label" />
-          <TextField label="Some Label" />
-        </FormRow>
-      </FormFieldSet>
-    </FormWrapper>
-  );
-});
+const onCancel = () => {
+  console.log('onCancel');
+};
+storiesOf('Forms|FormWrapper', module)
+  .add('default', () => {
+    return (
+      <FormWrapper
+        submitLabel="Submit"
+        cancelLabel="Cancel"
+        onSubmit={onSubmit}
+        onCancel={onCancel}
+      >
+        <FormFieldSet title="Lorem ipsum">
+          <FormRow gutterBottom={true}>
+            <TextField label="Some Label" />
+            <TextField label="Some Label" />
+          </FormRow>
+          <FormRow>
+            <TextField label="Some Label" />
+            <TextField label="Some Label" />
+          </FormRow>
+        </FormFieldSet>
+        <FormFieldSet title="Lorem ipsum">
+          <FormRow gutterBottom={true}>
+            <TextField label="Some Label" />
+            <TextField label="Some Label" />
+          </FormRow>
+          <FormRow>
+            <TextField label="Some Label" />
+            <TextField label="Some Label" />
+          </FormRow>
+        </FormFieldSet>
+      </FormWrapper>
+    );
+  })
+  .add('only submit', () => {
+    return (
+      <FormWrapper submitLabel="Login" onSubmit={onSubmit}>
+        <FormFieldSet title="Lorem ipsum">
+          <FormRow gutterBottom={true}>
+            <TextField label="Some Label" />
+            <TextField label="Some Label" />
+          </FormRow>
+          <FormRow>
+            <TextField label="Some Label" />
+            <TextField label="Some Label" />
+          </FormRow>
+        </FormFieldSet>
+      </FormWrapper>
+    );
+  });
