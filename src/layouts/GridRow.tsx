03d2b6d6d9bc9391784f343d79e0e26f74fc94cc
Fredrik Höntsch <fredrik.hoentsch@gmail.com>
2020-07-02 15:06:54 +0200

    added handling for missing columns

--- src/layouts/GridRow.tsx
+++ src/layouts/GridRow.tsx
@@ -11,56 +11,51 @@ export interface GridRowProps {
 }
 
 export const GridRow: React.FC<GridRowProps> = (props) => {
-  const { gridVariant = '12', children } = props;
+  const { gridVariant = '12' } = props;
+  const children = Array.isArray(props.children)
+    ? props.children
+    : [props.children];
 
   const renderGrid = () => {
-    if (children && Array.isArray(children) && children.length > 1) {
-      switch (gridVariant) {
-        case '6-6':
-          return children.map((child, index) => (
-            <Grid item key={index} xs={12} md={6}>
-              {child}
-            </Grid>
-          ));
-        case '4-4-4':
-          return children.map((child, index) => (
+    switch (gridVariant) {
+      case '6-6':
+        return children.map((child, index) => (
+          <Grid item key={index} xs={12} md={6}>
+            {child}
+          </Grid>
+        ));
+      case '4-4-4':
+        return children.map((child, index) => (
+          <Grid item key={index} xs={12} md={4}>
+            {child}
+          </Grid>
+        ));
+      case '3-3-3-3':
+        return children.map((child, index) => (
+          <Grid item key={index} xs={12} sm={6} md={3}>
+            {child}
+          </Grid>
+        ));
+      case '4-8':
+      case '8-4':
+        return children.map((child, index) => {
+          const i = gridVariant === '4-8' ? index : index + 1;
+          return i % 2 === 0 ? (
             <Grid item key={index} xs={12} md={4}>
               {child}
             </Grid>
-          ));
-        case '3-3-3-3':
-          return children.map((child, index) => (
-            <Grid item key={index} xs={12} sm={6} md={3}>
+          ) : (
+            <Grid item key={index} xs={12} md={8}>
               {child}
             </Grid>
-          ));
-        case '4-8':
-        case '8-4':
-          return children.map((child, index) => {
-            const i = gridVariant === '4-8' ? index : index + 1;
-            return i % 2 === 0 ? (
-              <Grid item key={index} xs={12} md={4}>
-                {child}
-              </Grid>
-            ) : (
-              <Grid item key={index} xs={12} md={8}>
-                {child}
-              </Grid>
-            );
-          });
-        default:
-          return children.map((child, index) => (
-            <Grid item key={index} xs={12}>
-              {child}
-            </Grid>
-          ));
-      }
-    } else {
-      return (
-        <Grid item xs={12}>
-          {children}
-        </Grid>
-      );
+          );
+        });
+      default:
+        return children.map((child, index) => (
+          <Grid item key={index} xs={12}>
+            {child}
+          </Grid>
+        ));
     }
   };
 
