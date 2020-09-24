2b26fa8149b0b107038c170cdecbb107f2d914f1
Fredrik Höntsch <fredrik.hoentsch@gmail.com>
2020-09-24 10:52:51 +0200

    [GWS-96] added headline option

--- src/components/Paper.spec.tsx
+++ src/components/Paper.spec.tsx
@@ -16,6 +16,12 @@ describe('<Paper />', () => {
     expect(getByTestId('paperContent').textContent).toBe('Lorem');
   });
 
+  it('should render the paper component with headline', () => {
+    const headline = 'Foo';
+    const { getByTestId } = render(<Paper headline={headline}>Lorem</Paper>);
+    expect(getByTestId('paper-headline').textContent).toBe(headline);
+  });
+
   it('should render the paper component with bottom margin', () => {
     const { getByTestId } = render(
       <Paper gutterBottom={true}>
