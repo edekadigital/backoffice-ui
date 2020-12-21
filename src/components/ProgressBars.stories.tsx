f91820301d457617ef0b5339c1ec2b08cd96ffa1
Antonia Krohn <antonia.krohn@edeka.de>
2020-12-21 12:42:16 +0100

    GWS Add tests

--- src/components/ProgressBars.stories.tsx
+++ src/components/ProgressBars.stories.tsx
@@ -1,8 +1,13 @@
+import { Button } from '@material-ui/core';
 import * as React from 'react';
-import { ProgressBarItem, ProgressBars } from './ProgressBars';
+import {
+  ProgressBarColors,
+  ProgressBarItem,
+  ProgressBars,
+} from './ProgressBars';
 
 export default {
-  title: 'Components/ProgressBarItem',
+  title: 'Components/ProgressBar',
   component: ProgressBars,
 };
 
@@ -11,10 +16,9 @@ const progressBars: ProgressBarItem[] = [
     headline: 'Lorem Ipsum',
     description: 'Ipsum',
     value: 10,
-    color: 'primary',
   },
-  { headline: 'Dolor', description: 'Ipsum', value: 40, color: 'primary' },
-  { headline: 'Sit Amet', description: 'Ipsum', value: 90, color: 'primary' },
+  { headline: 'Dolor', description: 'Ipsum', value: 40 },
+  { headline: 'Sit Amet', description: 'Ipsum', value: 90 },
 ];
 
 const progressBarsWithColors: ProgressBarItem[] = [
@@ -22,7 +26,6 @@ const progressBarsWithColors: ProgressBarItem[] = [
     headline: 'Lorem Ipsum',
     description: 'Ipsum',
     value: 10,
-    color: 'primary',
   },
   { headline: 'Dolor', description: 'Ipsum', color: 'success', value: 100 },
   { headline: 'Sit Amet', description: 'Ipsum', color: 'error', value: 100 },
@@ -34,4 +37,32 @@ export const Default = () => {
 
 export const WithErrorAndSuccess = () => {
   return <ProgressBars items={progressBarsWithColors} />;
+};
+
+export const WithChangingProps = () => {
+  const [value, setValue] = React.useState(0);
+
+  const items = React.useMemo(
+    () => [
+      {
+        headline: 'Headline',
+        description: `${value} von 100`,
+        value: value,
+        color: (value >= 50 ? 'success' : 'primary') as ProgressBarColors,
+      },
+    ],
+    [value]
+  );
+
+  return (
+    <>
+      <ProgressBars items={items} />
+      <Button onClick={() => (value < 100 ? setValue(value + 10) : null)}>
+        +
+      </Button>
+      <Button onClick={() => (value > 0 ? setValue(value - 10) : null)}>
+        -
+      </Button>
+    </>
+  );
 };
--- src/components/ProgressBars.tsx
+++ src/components/ProgressBars.tsx
@@ -3,8 +3,21 @@ import * as React from 'react';
 import { Body } from '../typography/Body';
 
 export interface ProgressBarsProps {
-  items: ProgressBarItem[];
+  /**
+   * Array of Progress Bars. Available Colors are primary, success and error
+   */
+  items: Array<ProgressBarItem>;
 }
+
+/**
+ * | Test ID                                 | Description                 |
+ * | --------------------------------------- | --------------------------- |
+ * | `progressBars`                          | container                   |
+ * | `progressBar-item-${index}`             | progress bar item           |
+ * | `progressBar-item-${index}-headline`    | headline of bar item        |
+ * | `progressBar-item-${index}-description` | bar description of bar item |
+ * | `progressBar-item-${index}-bar`         | actual bar of bar item      |
+ */
 
 export const ProgressBars: React.FC<ProgressBarsProps> = (props) => {
   const { items } = props;
@@ -15,12 +28,18 @@ export const ProgressBars: React.FC<ProgressBarsProps> = (props) => {
         description={item.description}
         value={item.value}
         color={item.color}
+        index={index}
       />
     </Grid>
   ));
 
   return (
-    <Grid container spacing={3} alignItems={'stretch'}>
+    <Grid
+      container
+      spacing={3}
+      alignItems={'stretch'}
+      data-testid="progressBars"
+    >
       {renderItems}
     </Grid>
   );
@@ -71,22 +90,34 @@ export interface ProgressBarItem {
   description?: string;
   value: number;
   color?: ProgressBarColors;
+  index?: number;
 }
 
-type ProgressBarColors = 'primary' | 'success' | 'error' | undefined;
+export type ProgressBarColors = 'primary' | 'success' | 'error' | undefined;
 
 const ProgressBarItem: React.FC<ProgressBarItem> = (props) => {
-  const { headline, description, value, color = 'primary' } = props;
+  const { headline, description, value, color = 'primary', index } = props;
   const classes = useProgressBarStyles({ color });
   return (
-    <div className={classes.container}>
+    <div
+      className={classes.container}
+      data-testid={`progressBar-item-${index}`}
+    >
       {headline ? (
-        <Body gutterBottom={1} variant="body2">
+        <Body
+          gutterBottom={1}
+          variant="body2"
+          data-testid={`progressBar-item-${index}-headline`}
+        >
           {headline}
         </Body>
       ) : null}
       {description ? (
-        <Body gutterBottom={1} variant="caption">
+        <Body
+          gutterBottom={1}
+          variant="caption"
+          data-testid={`progressBar-item-${index}-description`}
+        >
           {description}
         </Body>
       ) : null}
@@ -97,6 +128,7 @@ const ProgressBarItem: React.FC<ProgressBarItem> = (props) => {
         }}
         variant="determinate"
         value={value}
+        data-testid={`progressBar-item-${index}-bar`}
       />
     </div>
   );
