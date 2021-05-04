57881a9fa3ce60e5b56ac3c9198cd1e80f19de71
Antonia Krohn <antonia.krohn@edeka.de>
2021-05-04 19:40:21 +0200

    GWSWE-245 Add List Component

---
+++ src/components/List.stories.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { List } from './List';
+
+export default {
+  title: 'Components/List',
+  component: List,
+};
+
+export const Default = () => {
+  return <List />;
+};
